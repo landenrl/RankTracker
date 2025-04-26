d7c0a6c87eedb4196b75f321ac4b3679e40a7b26
landenrl <rogerslw@etsu.edu>
2025-04-26 17:45:37 -0400

    QOL
    Used Bootstrap to make the website look "prettier", and added collections of games and rankentries to applicationdbuser so that we can find the relationship in the database.

---
+++ RankTracker/Migrations/20250426211503_ApplicationUserUpdate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RankTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationUserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "RankEntries",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RankEntries_ApplicationUserId",
+                table: "RankEntries",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RankEntries_AspNetUsers_ApplicationUserId",
+                table: "RankEntries",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RankEntries_AspNetUsers_ApplicationUserId",
+                table: "RankEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RankEntries_ApplicationUserId",
+                table: "RankEntries");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "RankEntries");
+        }
+    }
+}
--- RankTracker/Models/ApplicationUser.cs
+++ RankTracker/Models/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace RankTracker.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public ICollection<RankEntry> RankEntries { get; set; } = new List<RankEntry>();
+        public ICollection<Game> Games { get; set; } = new List<Game>();
     }
 }
