a246b596acb9d0ecd3271080822fe3187acf7077
landenrl <rogerslw@etsu.edu>
2025-04-29 19:04:50 -0400

    Game Id Fix
    Fixed an error on the dashboard page where it incorrectly said it was displaying the Game Name rather than the Game Id. I changed it to correctly state it is displaying the game Id.

--- RankTracker/obj/Debug/net8.0/RankTracker.AssemblyInfo.cs
+++ RankTracker/obj/Debug/net8.0/RankTracker.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RankTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aeebd493916bb0b163c603b45a6fd5956c62433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c4ac60255f9b80992969a77846cee08d7405e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RankTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RankTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
